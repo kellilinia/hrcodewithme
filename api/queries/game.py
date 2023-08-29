65d98ce9f35a4aa43596493f554f1b8f551fe9d6
Silvano <silgon10@gmail.com>
2023-08-29 10:09:22 -0700

    added error handling to endpoints

--- api/queries/game.py
+++ api/queries/game.py
@@ -22,84 +22,89 @@ class GameOut(BaseModel):
 
 class GameQueries:
     def create(self, game: GameIn) -> GameOut:
-        # connect to db
-        with pool.connection() as conn:
-            # get a cursor(something to run sql in)
-            with conn.cursor() as db:
-                # run our insert
-                result = db.execute(
-                    """
-                    INSERT INTO Game
-                        (language, question, answer, wrong_answer, difficulty)
-                    VALUES
-                        (%s, %s, %s, %s, %s)
-                        RETURNING   id;
-                    """,
-                    [
-                        game.language,
-                        game.question,
-                        game.answer,
-                        game.wrong_answer,
-                        game.difficulty,
-                    ],
-                )
-                id = result.fetchone()[0]
-                # return new data
-                old_data = game.dict()
-                return GameOut(id=id, **old_data)
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO Game
+                            (language, question, answer, wrong_answer, difficulty)
+                        VALUES
+                            (%s, %s, %s, %s, %s)
+                            RETURNING id;
+                        """,
+                        [
+                            game.language,
+                            game.question,
+                            game.answer,
+                            game.wrong_answer,
+                            game.difficulty,
+                        ],
+                    )
+                    id = result.fetchone()[0]
+                    old_data = game.dict()
+                    return GameOut(id=id, **old_data)
+        except Exception as e:
+            print(e)
+            return {"message": "unable to create game"}
 
     def get_all(self) -> List[GameOut]:
         games = []
-        # connect to db
-        with pool.connection() as conn:
-            # get a cursor
-            with conn.cursor() as db:
-                # execute the query
-                db.execute(
-                    """
-                    SELECT id, language, question, answer, wrong_answer, difficulty
-                    FROM Game;
-                    """
-                )
-                # fetch all rows
-                rows = db.fetchall()
-                for row in rows:
-                    game = GameOut(
-                        id=row[0],
-                        language=row[1],
-                        question=row[2],
-                        answer=row[3],
-                        wrong_answer=row[4],
-                        difficulty=row[5],
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id, language, question, answer, wrong_answer, difficulty
+                        FROM Game;
+                        """
                     )
-                    games.append(game)
+                    rows = db.fetchall()
+                    for row in rows:
+                        game = GameOut(
+                            id=row[0],
+                            language=row[1],
+                            question=row[2],
+                            answer=row[3],
+                            wrong_answer=row[4],
+                            difficulty=row[5],
+                        )
+                        games.append(game)
+        except Exception as e:
+            print(e)
+            return {"message": "unable to get all games"}
         return games
 
     def get_by_id(self, game_id: int) -> Optional[GameOut]:
-        # connect to db
-        with pool.connection() as conn:
-            # get a cursor
-            with conn.cursor() as db:
-                # execute the query
-                db.execute(
-                    """
-                    SELECT id, language, question, answer, wrong_answer, difficulty
-                    FROM Game
-                    WHERE id = %s;
-                    """,
-                    [game_id],
-                )
-                # fetch the row
-                row = db.fetchone()
-                if row is not None:
-                    game = GameOut(
-                        id=row[0],
-                        language=row[1],
-                        question=row[2],
-                        answer=row[3],
-                        wrong_answer=row[4],
-                        difficulty=row[5],
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id,
+                        language,
+                        question,
+                        answer,
+                        wrong_answer,
+                        difficulty
+                        FROM Game
+                        WHERE id = %s;
+                        """,
+                        [game_id],
                     )
-                    return game
-                else:
-                    return None
+                    row = db.fetchone()
+                    if row is not None:
+                        game = GameOut(
+                            id=row[0],
+                            language=row[1],
+                            question=row[2],
+                            answer=row[3],
+                            wrong_answer=row[4],
+                            difficulty=row[5],
+                        )
+                        return game
+                    else:
+                        return None
+        except Exception as e:
+            print(e)
+            return {"message": "unable to get game"}
