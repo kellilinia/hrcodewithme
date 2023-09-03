202f87d0b8bf4a63380b171472d2578bfd516228
Jaime Huey <jaimehuey12@gmail.com>
2023-09-03 01:35:53 +0000

    Coder signup toggle

--- ghi/src/SignupForm.js
+++ ghi/src/SignupForm.js
@@ -1,43 +1,41 @@
 import { useState } from "react";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 import { useNavigate } from "react-router-dom";
-
+import "./toggle.css";
 
 const SignupForm = () => {
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [first_name, setFirstName] = useState("");
-    const [last_name, setLastName] = useState("");
-    // const [coder, setCoder] = useState(true);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [coder, setCoder] = useState(true);
 
-    const { register } = useToken();
-    const navigate = useNavigate();
+  const { register } = useToken();
+  const navigate = useNavigate();
 
   const handleRegistration = (e) => {
     e.preventDefault();
     const accountData = {
-        email: email,
-        username: username,
-        password: password,
-        first_name: first_name,
-        last_name: last_name,
-        // coder: coder,
+      email: email,
+      username: username,
+      password: password,
+      first_name: first_name,
+      last_name: last_name,
+      coder: coder,
     };
-    register(
-      accountData,
-      `${process.env.REACT_APP_API_HOST}/accounts`
-    );
+    register(accountData, `${process.env.REACT_APP_API_HOST}/accounts`);
     e.target.reset();
     navigate("/");
   };
+
   return (
     <div className="card text-bg-light mb-3">
       <h5 className="card-header">Signup</h5>
       <div className="card-body">
         <form onSubmit={(e) => handleRegistration(e)}>
-        <div className="mb-3">
-            <label className="form-label">email</label>
+          <div className="mb-3">
+            <label className="form-label">Email Address</label>
             <input
               name="email"
               type="text"
@@ -48,7 +46,7 @@ const SignupForm = () => {
             />
           </div>
           <div className="mb-3">
-            <label className="form-label">username</label>
+            <label className="form-label">Username</label>
             <input
               name="username"
               type="text"
@@ -59,7 +57,7 @@ const SignupForm = () => {
             />
           </div>
           <div className="mb-3">
-            <label className="form-label">password</label>
+            <label className="form-label">Password</label>
             <input
               name="password"
               type="password"
@@ -70,7 +68,7 @@ const SignupForm = () => {
             />
           </div>
           <div className="mb-3">
-            <label className="form-label">first name</label>
+            <label className="form-label">First name</label>
             <input
               name="first"
               type="text"
@@ -81,7 +79,7 @@ const SignupForm = () => {
             />
           </div>
           <div className="mb-3">
-            <label className="form-label">last name</label>
+            <label className="form-label">Last name</label>
             <input
               name="last"
               type="text"
@@ -91,17 +89,22 @@ const SignupForm = () => {
               }}
             />
           </div>
-          {/* <div className="mb-3">
-            <label className="form-label">coder</label>
-            <input
-              name="coder"
-              type="text"
-              className="form-control"
-              onChange={(e) => {
-                setCoder(e.target.value);
-              }}
-            />
-          </div> */}
+          <p>
+            <b>Are you a coder?</b>
+          </p>
+          <div>
+            <label className="switch">
+              <input
+                type="checkbox"
+                onChange={() => {
+                  setCoder(!coder);
+                }}
+                defaultChecked={true}
+              />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <p></p>
           <div>
             <input className="btn btn-primary" type="submit" value="Register" />
           </div>
